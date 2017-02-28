a377161d50f9ac9296477d88acdc338ee74fd19e
wudyijava <wudayong0703@gmail.com>
2017-02-28 14:16:14 +0800

    PositionEscape：通过C++组件打印出它依附Actor的坐标位置 OpenDoor：通过门上的C++组件判定DefaultPawn是否进入碰撞体，以此触发门的旋转打开

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+	}
+}
+
+void UOpenDoor::OpenDoor() {
+	AActor* owner = GetOwner();
+	FRotator newRotator = FRotator(0.0f, openAngle, 0.0f);
+	owner->SetActorRotation(newRotator);
+}
+
